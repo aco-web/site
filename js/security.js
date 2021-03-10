40771a462e11d44a5e08c8096cc215efcc8f5e14
Ayaan-Codes <64687788+Ayaan-Codes@users.noreply.github.com>
2021-03-10 09:51:01 +0300

    js

--- js/security.js
+++ js/security.js
@@ -36,4 +36,17 @@ return false;
 }
 return true;
 }
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function() {
+    var href = $.attr(this, 'href');
+
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+
+    return false;
+});
 </script>
