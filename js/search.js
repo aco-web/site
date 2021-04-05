5c08f2f43cb0840e95dac1f7d534cbb96fb822e6
Top-Of-Tech <67748521+Top-Of-Tech@users.noreply.github.com>
2021-04-04 19:55:20 -0500

    Added search functionality

--- js/search.js
+++ js/search.js
@@ -1,6 +1,50 @@
-const cards = document.getElementsByClassName("card");
+const search = new URLSearchParams(window.location.search);
+const search_query = params.get("search");
+const card_slots = document.querySelectorAll(".card");
 
-let card_content = [["<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTlYvzt71tyiUOpi5T6DK7z2KpNo6wMQpTrAg&usqp=CAU' alt='Denim Jeans' style='width:100%'>",
+const products = [
+    "ACO Online Security & Ad Blocker",
+    "ACO Search",
+    "ACO Calculator",
+    "ACO Premium",
+    "ACO SMARTi app",
+    "ACO Upload Magic",
+    "ACO Network Tester",
+    "ACO Drive",
+    "ACO Work ECO-System",
+    "ACO Development",
+    "ACO Games",
+    "ACO Statistics",
+    "ACO Meet",
+    "ACO Store",
+    "ACO Support System",
+    "ACO Chat",
+    "ACO Mail",
+    "ACO BOT",
+    "ACO Photo",
+    "ACO Oauth SignIn",
+    "ACO Verification System",
+    "ACO Cast",
+    "ACO Accounts",
+    "ACO ShareUp",
+    "ACO Translate",
+    "ACO Open",
+    "ACO IT",
+    "ACO Multi",
+    "ACO Shopper",
+    "ACO AutoCore",
+    "ACO SpellCheck",
+    "ACO AI Shop",
+    "ACO LiveStat",
+    "ACO Blog",
+    "ACO Ask",
+    "ACO WebModeration",
+    "ACO Developer Tools",
+    "ACO WHAT?",
+    "ACO Maps",
+]
+
+let card_slot_content = [["<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTlYvzt71tyiUOpi5T6DK7z2KpNo6wMQpTrAg&usqp=CAU' alt='Denim Jeans' style='width:100%'>",
                     "<h1>ACO Online Security & Ad Blocker</h1>",
                     "<p class='price'>ACO Tech</p>",
                     "<p>ACO Online Security and Ad blocker! The best free secure service online!</p>",
@@ -77,7 +121,7 @@ let card_content = [["<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%
                  "<h1>ACO Meet</h1>",
                  "<p class='price'>ACO Social</p>",
                  "<p>HD video and voice meetings for FREE. Get ACO MEET (beta) today and start connecting you team!</p>",
-                 "<button onclick='window.location.href=`mailto:a.co.network01@gmail.com`';>Request beta!</button>"],
+                 "<button onclick='window.location.href=`mailto:a.co.network01@gmail.com`;;>Request beta!</button>"],
                  
                     ["<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRUPThBalpSHOLEopPZcckZ1JrgYYZddJJtFQ&usqp=CAU' alt='Denim Jeans' style='width:100%'>",
                  "<h1>ACO Store</h1>",
@@ -236,19 +280,47 @@ let card_content = [["<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%
                   "<p>Navigate the world in 2d or 3d maps that update every week! Become a ACO assistant and add places to the map for everyone else to enjoy! Find something about a place already on the map? Well write a reveiw about it!</p>",
                   "<button onclick='window.location.href=`mailto:a.co.network01@gmail.com`;'>Request beta!</button>"]];
 
-function shuffle(array) {
-   array.sort(() => Math.random() - 0.5);
+function gnomeSort(arr, n)
+{
+    let index = 0;
+
+    while (index < n) {
+        if (index == 0)
+            index++;
+        if (arr[index][1] >= arr[index - 1][1])
+            index++;
+        else {
+            let temp = 0;
+            temp = arr[index];
+            arr[index] = arr[index - 1];
+            arr[index - 1] = temp;
+            index--;
+        }
+    }
+    return;
 }
-                
-shuffle(card_content);
-const params = new URLSearchParams(window.location.search);
 
-const searcha = params.get("search");
-
-if (searcha == null) {
-   for (i = 0; i < cards.length; i++) {
-      for (y = 0; y < card_content[i].length; y++) {
-         cards[i].innerHTML += card_content[i][y];
+let matches = []
+if (search_query != null) {
+   for (i = 0; i < products.length; i++) {
+      const search_index = products[i].toLowerCase().search(search_query.toLowerCase());
+      if (search_index != -1) {
+          matches.push([i, search_index]);
       }
+   }
+   if (matches.length != 1) {
+      gnomeSort(matches, matches.length)
+   }
+  
+   for (i = 0; i < matches.length; i++) {
+      for (y = 0; y < card_slot_content[i].length; y++) {
+          card_slots[i].innerHTML += card_slot_content[matches[i][0]][y];
+      }
+   }
+}
+
+for (i = 0; i < card_slots.length; i++) {
+   if (card_slots[i].childNodes.length < 2) {
+      card_slots[i].remove();
    }
 }
