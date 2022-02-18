7bc9e0da225ed1663950c4f24372e7ce6d54cf1b
itwithlyam <lyam.mosnier@itwithlyam.co.uk>
2022-02-18 13:22:24 +0000

    Move existing styles

--- login/login.php
+++ login/login.php
@@ -95,10 +95,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <title>Login</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
     <link rel="stylesheet" href="../css/accounts.css">
 </head>
 <body>
