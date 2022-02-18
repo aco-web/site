28681b547fa30609d335ab8e1a8befb2aec880dc
itwithlyam <lyam.mosnier@itwithlyam.co.uk>
2022-02-18 13:43:48 +0000

    fix reset password

--- login/reset-password.php
+++ login/reset-password.php
@@ -83,15 +83,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <p>Please fill out this form to reset your password.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
             <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
-                <label>New Password</label>
+                <label>New Password</label><br>
                 <input type="password" name="new_password" class="form-control" value="<?php echo $new_password; ?>">
                 <span class="help-block"><?php echo $new_password_err; ?></span>
-            </div>
+            </div><br>
             <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <label>Confirm Password</label>
+                <label>Confirm Password</label><br>
                 <input type="password" name="confirm_password" class="form-control">
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
-            </div>
+            </div>><br>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
                 <a class="btn btn-link" href="welcome.php">Cancel</a>
