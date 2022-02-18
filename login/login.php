5459e6bafdb07591315f84ecc43bdb88c179857e
itwithlyam <lyam.mosnier@itwithlyam.co.uk>
2022-02-18 13:23:42 +0000

    How about this

--- login/login.php
+++ login/login.php
@@ -94,7 +94,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="../css/accounts.css">
 </head>
 <body>
